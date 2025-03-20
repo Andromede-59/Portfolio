e532997d910dded59ccde500de397456e3dd699e
Julien MATTOT <julien.mattot.etu@univ-lille.fr>
2025-03-20 09:56:42 +0100

    <FIX> footer date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="bg-gray-800 text-white p-4">
             <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
                 <div className="text-center md:text-left">
-                    <p>&copy; {(new Date).getFullYear()} Julien MATTOT. {t("allRightsReserved")}</p>
+                    <p>&copy; {(new Date()).getFullYear()} Julien MATTOT. {t("allRightsReserved")}</p>
                 </div>
                 <div className="flex space-x-4 mt-4 md:mt-0">
                     <a 
